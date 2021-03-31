17c199bd51d828a278731921bb28e82cc961ca67
Tama McGlinn <t.mcglinn@gmail.com>
2021-03-31 21:37:46 +0200

    Add binding for bcm2835_init

--- src/ada_raspio.adb
+++ src/ada_raspio.adb
@@ -1,8 +1,14 @@
+with bcm2835_h;
+with Interfaces.C; use Interfaces.C;
+
 package body Ada_Raspio is
 
    procedure Initialize is
+      Error_Code : constant Interfaces.C.int := bcm2835_h.bcm2835_init;
    begin
-      null;
+      if Error_Code /= 0 then
+         raise Initialization_Error with "BCM2835 initialization failed";
+      end if;
    end Initialize;
 
 end Ada_Raspio;
--- src/ada_raspio.ads
+++ src/ada_raspio.ads
@@ -1,5 +1,7 @@
 package Ada_Raspio is
 
+   Initialization_Error : exception;
+
    procedure Initialize;
 
 end Ada_Raspio;
