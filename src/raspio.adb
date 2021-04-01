4fa104f1db2feb2601cd0e33c193562baf1c38c5
Tama McGlinn <t.mcglinn@gmail.com>
2021-04-01 12:55:46 +0200

    Fixed initialization call; 1 means success

--- src/raspio.adb
+++ src/raspio.adb
@@ -3,12 +3,12 @@ with Interfaces.C; use Interfaces.C;
 
 package body Raspio is
 
-   procedure Initialize is
-      Error_Code : constant Interfaces.C.int := bcm2835_h.bcm2835_init;
-   begin
-      if Error_Code /= 0 then
-         raise Initialization_Error with "BCM2835 initialization failed";
-      end if;
-   end Initialize;
+  procedure Initialize is
+    Error_Code : constant Interfaces.C.int := bcm2835_h.bcm2835_init;
+  begin
+    if Error_Code /= 1 then
+      raise Initialization_Error with "BCM2835 initialization failed";
+    end if;
+  end Initialize;
 
 end Raspio;
