a2dd35bd700f5f9a1730d4d0bc354d6dff2ed2d4
Tama McGlinn <t.mcglinn@gmail.com>
2021-04-04 11:00:35 +0200

    Fixed formatting

--- src/ow.adb
+++ src/ow.adb
@@ -3,28 +3,27 @@ with Raspio.GPIO;
 with Ada.Real_Time;
 
 procedure Ow is
-  use Raspio.GPIO;
-  use type Ada.Real_Time.Time;
-  use type Ada.Real_Time.Time_Span;
+   use Raspio.GPIO;
+   use type Ada.Real_Time.Time;
+   use type Ada.Real_Time.Time_Span;
 
-  Poll_Time : Ada.Real_Time.Time := Ada.Real_Time.Clock;
-  Period : constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds (1000);
+   Poll_Time : Ada.Real_Time.Time               := Ada.Real_Time.Clock;
+   Period    : constant Ada.Real_Time.Time_Span :=
+     Ada.Real_Time.Milliseconds (1_000);
 
 begin
-  Raspio.Initialize;
-  declare
-    Button : constant Raspio.GPIO.Pin_Type :=
-      Raspio.GPIO.Create
-        (Pin_ID           => RPI_GPIO_P1_12, Mode => Input,
-        Internal_Resistor => Pull_Up);
-  begin
-    loop
-      if Raspio.GPIO.Read (Button) = On then
-        Ada.Text_IO.Put_Line ("Ow!");
-      end if;
-      delay until Poll_Time;
-      Poll_Time := Poll_Time + Period;
-    end loop;
-  end;
+   Raspio.Initialize;
+   declare
+      Button : constant Raspio.GPIO.Pin_Type :=
+        Raspio.GPIO.Create
+          (Pin_ID => GPIO_P1_12, Mode => Input, Internal_Resistor => Pull_Up);
+   begin
+      loop
+         if Raspio.GPIO.Read (Button) = On then
+            Ada.Text_IO.Put_Line ("Ow!");
+         end if;
+         delay until Poll_Time;
+         Poll_Time := Poll_Time + Period;
+      end loop;
+   end;
 end Ow;
-
