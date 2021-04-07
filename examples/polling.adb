c0252aa17a1b63adb524d5a78bc1da56859afcc8
Tama McGlinn <t.mcglinn@gmail.com>
2021-04-07 18:57:42 +0200

    Add gpr build method for polling.adb example

--- examples/polling.adb
+++ examples/polling.adb
@@ -2,23 +2,21 @@ with Ada.Text_IO;
 with Raspio.GPIO;
 
 procedure Polling is
-  use Raspio.GPIO;
+   use Raspio.GPIO;
 begin
-  Raspio.Initialize;
-  declare
-    -- connect button from pin 12 to ground
-    Button : constant Pin_Type := Create (
-      Pin_ID => GPIO_P1_12,
-      Mode => Input,
-      Internal_Resistor => Pull_Up);
+   Raspio.Initialize;
+   declare
+      --  connect button from pin 12 to ground
+      Button : constant Pin_Type :=
+        Create
+          (Pin_ID => GPIO_P1_12, Mode => Input, Internal_Resistor => Pull_Up);
 
-    State : Pin_State := Off;
-  begin
-    loop
-      State := Read (Button);
-      Ada.Text_IO.Put_Line (State'Image);
-      delay 0.2;
-    end loop;
-  end;
+      State : Pin_State := Off;
+   begin
+      loop
+         State := Read (Button);
+         Ada.Text_IO.Put_Line (State'Image);
+         delay 0.2;
+      end loop;
+   end;
 end Polling;
-
